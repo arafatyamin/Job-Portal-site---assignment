65dc0a6420d19cd81c030d93a8d49ec6561af6c8
arafatyamin <mdh631987@gmail.com>
2023-02-18 10:40:45 +0600

    daynamic data fetching

--- src/pages/TopJobCategories/Experiences.jsx
+++ src/pages/TopJobCategories/Experiences.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Experiences = () => {
+    const [data,setData] = useState([])
+    useEffect(() => {
+        fetch('Experienced_jobs.json')
+        .then(res => res.json())
+        .then(data => setData(data))
+    },[])
+    console.log(data)
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mx-8'>
+            {
+                data.map(job => <div key={job.id} class="">
+                <div class=" bg-white flex items-center space-x-6 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
+                  <div className='w-[20%]'>
+                    <img src={job.companies_logos} alt=""  />
+                  </div>
+                  <div>
+                    <h1 class="text-xl font-bold text-gray-700 mb-2">{job.position_name}</h1>
+                    <p class="text-gray-600 text-sm">{job.company_name}</p>
+                    <div className="flex justify-between">
+                    <small>{job.job_location}</small>
+                    <button className='btn'>Apply</button>
+                    </div>
+                  </div>
+                </div>
+              </div>)
+            }
         </div>
     );
 };
--- src/pages/TopJobCategories/Fresher.jsx
+++ src/pages/TopJobCategories/Fresher.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Fresher = () => {
+    const [data,setData] = useState([])
+    useEffect(() => {
+        fetch('Fresher_jobs.json')
+        .then(res => res.json())
+        .then(data => setData(data))
+    },[])
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-4'>
+           {
+            data.map(job => 
+                <div key={job.id} className="mb-4 border w-full">
+                <div><div className="p-6 bg-white flex items-center  rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
+                  
+                  <div>
+                    <h1 className="text-xl font-bold text-gray-700 mb-2">{job.company_name}</h1>
+                    <p className="text-gray-600">{job.position_name}</p>
+                    <small>{job.job_location}</small>
+                    <button>Apply</button>
+                  </div>
+                </div>
+                </div>
+              </div>
+              )
+           } 
         </div>
     );
 };
